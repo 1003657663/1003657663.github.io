7811f21f9ae2ba70ebe0fed0272c5fe80ef29c5e
chao <1003657663@qq.com>
2016-04-10 00:13:59 +0800

    change index

---
+++ newxueshengchu/js/indexBoth.js
@@ -0,0 +1,6 @@
+
+first();
+function first(){
+    var liWidth = $(".left-div .bd li").width();
+    $(".left-div .bd li p").width(liWidth -80);
+};
--- newxueshengchu/js/indexJs.js
+++ newxueshengchu/js/indexJs.js
@@ -3,60 +3,19 @@ var navigatorPanel;
 var skip,main,allskip,photoskip,infoskip;
 var infoLi;
 
-(function(){
-    var top;
-    var photo;
-    var photoHeight;
-	var list;
-	var picture;
-
-    setBackMargin();
-    window.onresize = setBackMargin;
-    setNavigator();
-    main = document.getElementById("navigatorskip");
-    allskip = main.getElementsByTagName("div");
-    window.onresize = setNavigator;
-    function setNavigator(){
-        list = document.getElementById("list");
-		picture = document.getElementById("picture");
-        navigatorLi = list.getElementsByTagName("li");
-		infoLi = picture.getElementsByTagName("li");
-    }
-})();
+setBackMargin();
 
 function setBackMargin(){
     document.getElementsByClassName("back").item(0).style.marginTop = document.body.offsetHeight * 0.4 + "px";
+
+    var width = $(".bottom-div").width();
+    var imgWidth = $(".bottom-img-div img").eq(0).width();
+    var toMargin = (width-imgWidth*4)/3;
+    $(".bottom-div .bottom-img-div").each(function (index,element) {
+        console.info(index);
+        console.info(element);
+        if(index!=0){
+            element.style.marginLeft = toMargin + 'px';
+        }
+    });
 }
-
-function changeLi(e){
-    for(i=0;i<navigatorLi.length;i++)
-    {
-         navigatorLi[i].style.background = "rgba(255,255,255,1)";
-		 navigatorLi[i].style.color = "rgba(0,0,0,1)";
-    }
-    for(h=0;h<allskip.length;h++)
-    {
-        allskip[h].style.display = "none";
-    }
-
-    e.style.backgroundColor = "rgba(25,101,255,1)";
-	e.style.color = "rgba(255,255,255,1)";
-    skip = document.getElementById(e.id+"skip");
-    skip.style.display = "block";
-}
-function varyLi(e){
-    for(i=0;i<infoLi.length;i++)
-    {
-        infoLi[i].style.background = "rgba(255,255,255,1)";
-		 infoLi[i].style.color = "rgba(0,0,0,1)";
-    }
-    for(h=1;h<=3;h++)
-    {
-		skip = document.getElementById("infoLi"+h+"skip").style.display="none";
-    }
-
-    e.style.backgroundColor = "rgba(25,101,255,1)";
-	e.style.color = "rgba(255,255,255,1)";
-    skip = document.getElementById(e.id+"skip");
-    skip.style.display = "block";
-};
