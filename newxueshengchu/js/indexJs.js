3d2db6ad6608b2a41dd84c47564353824af141cc
chao <1003657663@qq.com>
2016-04-13 15:29:20 +0800

    new

--- newxueshengchu/js/indexJs.js
+++ newxueshengchu/js/indexJs.js
@@ -1,32 +1,30 @@
-var navigatorLi;
-var navigatorPanel;
-var skip, main, allskip, photoskip, infoskip;
-var infoLi;
 
 setBackMargin();
 
 function setBackMargin() {
     $(".bottom-div").css("margin","100px 0 0 0");
+    document.getElementsByClassName("back").item(0).style.marginTop = document.body.offsetHeight * 0.4 + "px";
     if (document.body.offsetWidth > 750) {
         $(".bottom-div .bottom-img-div").each(function(index,element){
             element.style.width = '163px';
             element.style.height = '100px';
         });
-        console.info(">750");
+
+
+        var width = $(".bottom-div").width();
+        var imgWidth = 163;//$(".bottom-img-div img").eq(0).width();
+        var toMargin = (width - imgWidth * 4) / 3;
+        $(".bottom-div .bottom-img-div").each(function (index, element) {
+            if (index != 0) {
+                element.style.marginLeft = toMargin + 'px';
+            }
+        });
+
     } else {
         $(".bottom-div .bottom-img-div").each(function(index,element){
-            element.style.width = '98px';
-            element.style.height = '60px';
+            /*element.style.width = '98px';
+            element.style.height = '60px';*/
+            element.style.margin = '0';
         });
     }
-    document.getElementsByClassName("back").item(0).style.marginTop = document.body.offsetHeight * 0.4 + "px";
-
-    var width = $(".bottom-div").width();
-    var imgWidth = $(".bottom-img-div img").eq(0).width();
-    var toMargin = (width - imgWidth * 4) / 3;
-    $(".bottom-div .bottom-img-div").each(function (index, element) {
-        if (index != 0) {
-            element.style.marginLeft = toMargin + 'px';
-        }
-    });
 }
